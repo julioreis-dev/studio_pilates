56634e1141d449f8ef4d436b3760e937abefbab5
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-11-26 21:16:08 -0300

    improvements in views_index.py

--- alunos/views/views_index.py
+++ alunos/views/views_index.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 class IndexTemplateView(TemplateView):
     template_name = 'home/home.html'
     # template_name = 'inner-page.html'
