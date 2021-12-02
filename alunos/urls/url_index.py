45318d83e337d09baa5d014d3e919c8b9d415c98
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-12-02 16:25:05 -0300

    The file url_index.py received comments

--- alunos/urls/url_index.py
+++ alunos/urls/url_index.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from alunos.views.views_index import IndexTemplateView
 
-"""
-URL que acessa o template index.html por meio da classbasedview IndexTemplateView
-"""
+
+# URL que acessa o template index.html por meio da classbasedview IndexTemplateView
 
 app_name = 'index'
 
