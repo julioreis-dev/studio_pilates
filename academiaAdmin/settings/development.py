f4abbdb4367741d86a0b57e1d16cb35d33529ac2
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-12-11 11:05:21 -0300

    set whole project to production using production.py

--- academiaAdmin/settings/development.py
+++ academiaAdmin/settings/development.py
@@ -10,13 +10,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
--- academiaAdmin/settings/production.py
+++ academiaAdmin/settings/production.py
@@ -1,4 +1,6 @@
 from academiaAdmin.settings.settings import *
+import dj_database_url
+
 
 DEBUG = True
 
@@ -8,8 +10,5 @@ SECRET_KEY = 'n#8(pi2zl9d44k!lo2z46%zm-5%uagyo+=r-fy9muyw13+)ow@'
 ALLOWED_HOSTS = ['pilatesapp.herokuapp.com']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
