f8076b0fea75381c4da4db016ebeb3367512be8f
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-12-11 10:43:11 -0300

    improvements in the settings.py was included postgres database

--- academiaAdmin/settings/development.py
+++ academiaAdmin/settings/development.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from academiaAdmin.settings.settings import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -8,9 +10,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
